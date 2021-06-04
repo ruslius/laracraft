4246172a3598f03cba3ad50eb29ea6752aa40667
RUSLAN <49198@bk.ru>
2021-06-04 13:46:15 +0300

    test

--- src/HelperClient.php
+++ src/HelperClient.php
@@ -2,14 +2,26 @@
 
 namespace Ruslius\Helper;
 
+use Illuminate\Support\Facades\Log;
+
 class HelperClient
 {
-	public static function dump($arr){
+	protected $construct;
+
+	public function __construct()
+	{
+		Log::info("Был выполнен __construct");
+	}
+
+	public static function preEcho($arr){
 		echo("<pre>");
 			print_r($arr);
 		echo("</pre>");
 	}
 	public static function getInfo(){
 		return config()->all();
+	}
+	public static function ifConstruct(){
+		//return $this->construct;
 	}
 }
